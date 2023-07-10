a1e2138f29b58bbf56fec664d54b892347f728fd
Hridoy <ridoy.java@gmail.com>
2023-07-10 13:59:11 +0600

    JSONparse

---
+++ JSONparse.js
@@ -0,0 +1,6 @@
+const jsonString = '{"name": "John", "age": 30, "city": "New York"}';
+const obj = JSON.parse(jsonString);
+
+console.log(obj.name); // Output: John
+console.log(obj.age); // Output: 30
+console.log(obj.city); // Output: New York

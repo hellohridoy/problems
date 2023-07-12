87c8bbaf66e7ea4c3c1a573bed4c629896f390ba
Hridoy <ridoy.java@gmail.com>
2023-07-12 10:46:33 +0600

    add number and even odd

--- map.js
+++ map.js
@@ -1,4 +1,15 @@
-const myArray = ['apple', 'banana', 'orange'];
+const array = [1,2,3,4,5];
 
-const myList = myArray.map((item) => {item});
-console.log(myList);
+var answer = 0;
+
+array.map(ele =>{
+    answer = answer + ele;
+});
+console.log(answer)
+
+
+array.map(ele =>{
+    if(ele%2==!0){
+        console.log(ele);
+    }
+})

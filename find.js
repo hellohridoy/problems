492c1c23acdc6b74cee5e26cac8d7efecb93cbc2
Hridoy <ridoy.java@gmail.com>
2023-07-12 11:44:55 +0600

    find method return first number from array

---
+++ find.js
@@ -0,0 +1,7 @@
+const array = [1,2,3,4,5,6,67,88,99,5656];
+let findFromArray = array.find(item => item>50)
+console.log(findFromArray);
+
+let number = [-10, -0.20, 0.30, -40, -50];
+let found = number.find(n => n>0)
+console.log(found);

3eece3ac83f595505778d3fc3cbd930fb402ba88
Hridoy <ridoy.java@gmail.com>
2023-07-13 14:55:07 +0600

    callBackFunction

---
+++ setTimeoutWithCallBack.js
@@ -0,0 +1,13 @@
+setTimeout(myFunction, 3000);
+
+function myFunction() {
+  console.log("I love You !!");
+}
+
+
+setInterval(myFunction,1000);
+
+function myFunction(){
+    let d = new Date();
+    console.log(d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
+}

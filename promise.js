da0ed5e8d8ca626f5a14a3d40f175f785fbd844b
Hridoy <ridoy.java@gmail.com>
2023-07-13 15:25:35 +0600

    promise

---
+++ promise.js
@@ -0,0 +1,49 @@
+function myDisplayer(some){
+    console.log(some)
+}
+let myPromise = new Promise(function(myResolve,myRecject){
+    let x = 0;
+    if(x==0){
+        myResolve("OK");
+    }else{
+        myRecject("Error");
+    }
+});
+
+myPromise.then(
+    function(value){myDisplayer(value)},
+    function(error){myDisplayer(error)}
+);
+
+
+
+let myPromiseTimeout = new Promise(function(myResolve,myRecject){
+    setTimeout(function(){myResolve("I love you")},3000)
+})
+
+myPromiseTimeout.then(function(value){
+    console.log(value);
+});
+
+
+// function myDisplayer(some) {
+//     document.getElementById("demo").innerHTML = some;
+//   }
+  
+//   let mPromise = new Promise(function(myResolve, myReject) {
+//     let req = new XMLHttpRequest();
+//     req.open('GET', "mycar.html");
+//     req.onload = function() {
+//       if (req.status == 200) {
+//         myResolve(req.response);
+//       } else {
+//         myReject("File not Found");
+//       }
+//     };
+//     req.send();
+//   });
+  
+//   myPromise.then(
+//     function(value) {myDisplayer(value);},
+//     function(error) {myDisplayer(error);}
+//   );

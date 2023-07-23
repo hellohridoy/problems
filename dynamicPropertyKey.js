00b742fe2d2178430456a4d315262b05045176c4
Hridoy <ridoy.java@gmail.com>
2023-07-23 18:27:52 +0600

    Dot notation, Bracket notation

---
+++ dynamicPropertyKey.js
@@ -0,0 +1,13 @@
+let me = {
+  name: "hridoy",
+};
+
+// 1. Dot notation
+console.log(me.name); // hridoy
+
+// 2. Bracket notation (string key)
+console.log(me["name"]); // hridoy
+
+// 3. Bracket notation (variable key)
+let key = "name";
+console.log(me[key]); // hridoy
